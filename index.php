5de5923146e5ca578b61cd4e27940aad202dd5fb
MikeSimon123 <mikesimonmikesimon@gmail.com>
2025-11-18 14:53:50 -0300

    pagina login

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Login</title>
+</head>
+<body>
+    <h1>Página de Login</h1>
+    <form action="" method="post">
+        <div id="formLogin">
+            <p>Email:</p> <input type="email" name="cxemail" id="email-login">
+            <p>Senha:</p> <input type="password" name="cxsenha" id="senha-login">
+        </div>
+        
+        <input type="submit" value="Entrar">
+    </form>
+</body>
+</html>
