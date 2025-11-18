209a74d2e6e6a743181382afb4107db6d558ba29
Felipe Ortega da Silva <eusouoortega@gmail.com>
2025-11-18 14:56:17 -0300

    teste2

--- config/connect.php
+++ config/connect.php
@@ -1,2 +1,9 @@
 <?php
-$user = "felipe";
+$user = "root";
+$host = "localhost";
+$password = "senha";
+$database = "ordem";
+$connection = mysqli_connect($host, $user, $password, $database);
+if($connection == false){
+    echo("Erro de conexão:". mysqli_connect_error($connection));
+}
